290dbf01b1deddf199d5b52ac5423bc0d7413b23
Manh ND <manh16nd@proton.me>
2025-02-16 13:07:01 +0700

    add logs

--- log/2502.ts
+++ log/2502.ts
@@ -5,15 +5,18 @@ interface Resource {
   tags: string[]
 }
 
-const codeBlock = (text: string): string => '```' + '\n' + text + '\n' + '```'
+const br = '\n'
+const codeBlock = (text: string): string => '```' + br + text + br + '```'
 
 export const data: Resource[] = [
   {
     id: '250216-air-env',
     title: 'Read .env with air',
     content:
-      `[Source](https://github.com/air-verse/air/issues/58#issuecomment-898537034)\n` +
-      `Update .air.yaml to this to make air reading argument from .env file:\n` +
+      `[Source](https://github.com/air-verse/air/issues/58#issuecomment-898537034)` +
+      br +
+      `Update .air.yaml to this to make air reading argument from .env file:` +
+      br +
       codeBlock(
         `[build]
 bin = ";export $(grep -v '^#' ..env | xargs); ./tmp/main"
