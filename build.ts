98b905d4253abcc988a17080e16db4b65a35144c
Manh ND <manh16nd@proton.me>
2025-02-16 16:10:41 +0700

    update build

--- build.ts
+++ build.ts
@@ -1,6 +1,16 @@
 import * as fs from 'fs'
 import * as path from 'path'
+import showdown from 'showdown'
 
+interface Resource {
+  id: string
+  title: string
+  content: string
+  tags: string[]
+  text: string
+}
+
+const converter = new showdown.Converter()
 const sourceDir = './source'
 const files: { fileName: string; content: string }[] = []
 
@@ -24,17 +34,42 @@ fs.mkdirSync('./log')
 
 const resource: string[] = []
 
+const fetched: Resource[] = []
 for (const f of files) {
   const fileId = f.fileName.replace(/\.md$/, '').replaceAll('/', '-')
-  console.log(fileId)
-  resource.push(fileId)
-  fs.writeFileSync(`./log/${fileId}`, f.content)
+  const response = fs.readFileSync(f.fileName, 'utf8')
+  const html = converter.makeHtml(response)
+  const lines = response.split('\n')
+  const firstLine = lines[0].replace(/^# */, '')
+
+  const content = converter.makeHtml(lines.slice(1).join('\n'))
+
+  const text = content
+    .replace(/<[^>]+>/g, '')
+    .replace(/&nbsp;/g, ' ')
+    .replace(/\s+/g, ' ')
+    .trim()
+
+  const title = firstLine
+  const tags = title.match(/\[(.+?)\]/)?.[1].split(',') || []
+
+  const resource = {
+    content,
+    id: fileId,
+    tags: tags.map(item => item.trim()),
+    title: title.split('[')[0].trim(),
+    text: text,
+  }
+
+  fs.writeFileSync(`./log/${fileId}.json`, JSON.stringify(resource, null, 2))
 }
+
+console.log(fetched)
 
 fs.writeFileSync(
   `./log/resource.json`,
   JSON.stringify(
-    resource.sort((a, b) => b.localeCompare(a)),
+    fetched.sort((a, b) => b.id.localeCompare(a.id)),
     null,
     2
   )
